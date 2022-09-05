055a02dcd1437fed847a5270bb39ee38dbee6e8d
canon20paul <canon20paul@gmail.com>
2022-09-05 14:07:28 +0300

    formating

--- mobile-menu.js
+++ mobile-menu.js
@@ -17,24 +17,11 @@ hamburger.addEventListener('click', () => {
     //     document.getElementById('names').style.zIndex = '-1';
         document.body.style.overflow = 'auto';
     //     document.getElementById('navB').style.opacity = '0';
-     }
+  }
 });
 // Hide navMenu
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navmenu.classList.remove('active');
-    // document.getElementById('email').style.zIndex = '1';
-    // document.getElementById('email').style.zIndex = '1';
-    // document.getElementById('names').style.zIndex = '1';
-     document.body.style.overflow = 'auto';
+  document.body.style.overflow = 'auto';
 }));
-
-
-
-// document.getElementById('contactF').addEventListener('submit', (event) => {
-//     const email = document.getElementById('email').value;
-//     if (email !== email.toLowerCase()) {
-//         event.preventDefault();
-//         document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
-//     }
-// })
