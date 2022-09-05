308cd5b55087c7d1134902938d1088d5fb369f15
canon20paul <canon20paul@gmail.com>
2022-09-05 09:53:28 +0300

    Mobile Modal

--- modal.js
+++ modal.js
@@ -1,10 +1,10 @@
 const projectsC = document.querySelector('.works');
 const works = [
     {
-        title: 'Multi-post Stories1 Gain+Glory',
+        title: 'Keeping track of hundreds of components',
         description:
             'A daily selection of privately Personalized reads; no accounts or Signups required, has been the industry" s standard dummy text eveer since the 1500s, when an unknown Printer took a standard dummy text.',
-        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
         featured_image: 'images/modal.png',
         lang_list: ['css', 'html', 'bootsrap', 'Ruby', 'Github', 'Codekit', 'Codepen'],
         source: 'https://github.com/canon20paul/My-Portfolio-setup-Desktop-version',
@@ -15,8 +15,8 @@ const works = [
         description:
             'A daily selection of privately Personalized reads; no accounts or Signups required, has been the industry" s standard dummy text eveer since the 1500s, when an unknown Printer took a standard dummy text.',
         detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
-        featured_image: 'images/modal.png',
-        lang_list: ['css', 'html', 'bootsrap', 'Ruby', 'Github', 'Codekit', 'Codepen'],
+        featured_image: 'images/5G.png',
+        lang_list: ['Github', 'Codekit', 'Codepen'],
         source: 'https://github.com/canon20paul/My-Portfolio-setup-Desktop-version',
         live_demo: 'https://canon20paul.github.io/My-Portfolio-setup-Desktop-version/',
     },
@@ -78,6 +78,18 @@ const languageHtml = (workObj, key, langList = '') => {
     /* eslint-enable no-restricted-syntax */
     return langList;
 };
+
+const languageHtml2 = (workObj, key, langList = '') => {
+    /* eslint-disable no-restricted-syntax */
+    for (const lang in workObj[key].lang_list) {
+        if (lang ) {
+            langList += `<li class="lang-list-item">${workObj[key].lang_list[lang]}</li>`;
+        }
+    }
+    /* eslint-enable no-restricted-syntax */
+    return langList;
+};
+
 let workCard ='' ;
 
 const appendWorks = (works) => {
@@ -89,7 +101,7 @@ const appendWorks = (works) => {
       <ul class="lang" id="card-languages">
     ${languageHtml(works, i)}
     </ul>
-    <button type="submit" class="buttonW">      
+    <button type="submit" class="buttonW" onclick="blurOut(), popUpDiv(${[i]}), href='#wrap-header';">
     See Project
     </button>
     </div>
@@ -100,36 +112,44 @@ appendWorks(works);
 projectsC.innerHTML = workCard;
 // eslint-disable-next-line no-unused-vars
 
-// function popUpDiv(j) {
-//     const wrap = document.createElement('section');
-//     const wrap1 = document.createElement('div');
-//     wrap.classList.add('wrap');
-//     wrap1.classList.add('wrap1');
-//     wrap1.innerHTML = ` <section class="wrap"><div class="wrap1">
-//   <div id="wrap-header" class="wrap-header"><h3>${works[j].title}</h3><div class="xx"><img class="xx-img" src="images/Cancel.png" alt="Close" onclick="removeBlur()" /></div></div>
-//   <div class="wrap1-img"><img class="wrap1-img-img" src="${works[j].featured_image}" alt="${works[j].title}" /></div>
-//   <div class="wrap-bottom"><p class="wrap-detail">${works[j].detail}</p>
-//   <ul >${languageHtml(works, j)}</ul>
-//   <div class="btnD"><button type="submit" id="live" class=" btnPP live"><a href="${works[j].live_demo}"><span class="spanL">See Live</span><img class="btnPP-imgL" src="images/live.png" alt=""></a></button> 
-//   <button type="submit" id="source" class="btnPP source"><a href="${works[j].source}"><span class="spanS">See Source</span><img class="btnPP-imgS" src="images/github.png" alt=""></a></button> </div>
-//   </div>
-//   <div class="Enclipe10"></div>
-//   <div class="Enclipe20"></div>
-//   </div>
-//   </section>`;
-//     wrap.appendChild(wrap1);
-//     document.body.appendChild(wrap);
-//     document.documentElement.scrollTop = 0;
-// }
-// // eslint-disable-next-line no-unused-vars
-// function blurOut() {
-//     const blur = document.querySelector('.wrapper');
-//     blur.setAttribute('id', 'blur');
-// }
-// // eslint-disable-next-line no-unused-vars
-// function removeBlur() {
-//     document.querySelector('.wrapper').removeAttribute('id');
-//     document.querySelector('.wrapper').removeAttribute('id');
-//     const shut = document.querySelector('.wrap');
-//     shut.remove();
-// }
+
+//modal functionality
+function popUpDiv(j) {
+    const wrap = document.createElement('section');
+    const wrap1 = document.createElement('div');
+    var cancel='images/cancel.png';
+
+    window.addEventListener('resize', ( ) => { if (window.innerWidth < 550) { cancel='images/Enable.png'; } return cancel; });
+    wrap.classList.add('wrap');
+    wrap1.classList.add('wrap1');
+    wrap1.innerHTML = ` <section class="wrap"><div class="wrap1">
+  <div class="xx"><img class="xx-img" src="${cancel}" alt="Close" onclick="removeBlur()" /></div>
+  <div class="wrap1-img"><img class="wrap1-img-img" src="${works[j].featured_image}" alt="${works[j].title}" /></div>
+  <div id="wrap-header" class="wrap-header"><h3>${works[j].title}</h3></div>
+  <div class="wrap-bottom">
+  <ul class="lang" id="card-languages">
+    ${languageHtml2(works, j)}
+    </ul>
+    <p class="wrap-detail">${works[j].detail}</p>
+  <div class="btnD"> <button type="submit" id="source" class="btnPP source"><a href="${works[j].source}"><span class="spanS">See Source</span><img class="btnPP-imgS" src="images/githubL.png" alt=""></a></button>
+  <button type="submit" id="live" class=" btnPP live"><a href="${works[j].live_demo}"><span class="spanL">See Live</span><img class="btnPP-imgL" src="images/live.png" alt=""></a></button> </div>
+  </div>
+ 
+  </div>
+  </section>`;
+    wrap.appendChild(wrap1);
+    document.body.appendChild(wrap);
+    document.documentElement.scrollTop = 0;
+}
+// eslint-disable-next-line no-unused-vars
+function blurOut() {
+    const blur = document.querySelector('.main-wrapper');
+    blur.setAttribute('id', 'blur');
+}
+// eslint-disable-next-line no-unused-vars
+function removeBlur() {
+    document.querySelector('.main-wrapper').removeAttribute('id');
+    document.querySelector('.main-wrapper').removeAttribute('id');
+    const shut = document.querySelector('.wrap');
+    shut.remove();
+}
