2f9e755ffe23b958a4f6662f34e64542c83037a1
canon20paul <canon20paul@gmail.com>
2022-09-06 19:42:49 +0300

    formating

--- modal.js
+++ modal.js
@@ -71,7 +71,7 @@ const works = [
 const languageHtml = (workObj, key, langList = '') => {
   /* eslint-disable no-restricted-syntax */
   for (const lang in workObj[key].lang_list) {
-    if (lang < 4 ){ langList += `<li class="lang-list-item">${workObj[key].lang_list[lang]}</li>`; }
+    if (lang < 4 ) { langList +=`<li class="lang-list-item">${workObj[key].lang_list[lang]}</li>`; }
   }
   /* eslint-enable no-restricted-syntax */
   return langList;
@@ -80,7 +80,7 @@ const languageHtml = (workObj, key, langList = '') => {
 const languageHtml2 = (workObj, key, langList = '') => {
   /* eslint-disable no-restricted-syntax */
   for (const lang in workObj[key].lang_list) {
-    if (lang ){ langList += `<li class="lang-list-item">${workObj[key].lang_list[lang]}</li>`; }
+    if (lang ) { langList +=`<li class="lang-list-item">${workObj[key].lang_list[lang]}</li>`;}
   }
   /* eslint-enable no-restricted-syntax */
   return langList;
