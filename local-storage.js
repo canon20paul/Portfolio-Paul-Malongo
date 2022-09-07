928cfd5fd6e019aff23a056fc9bdd0eda9211275
canon20paul <canon20paul@gmail.com>
2022-09-07 13:57:44 +0300

    formating

--- local-storage.js
+++ local-storage.js
@@ -1,21 +1,21 @@
 // This is for local storage
-const  contactForm = document.querySelector('#contactF');
+const contactForm = document.querySelector('#contactF');
 const client = {
-    names: '',
-    email: '',
-    message: ''
+  names: '',
+  email: '',
+  message: '',
 };
 
 contactForm.addEventListener('change', () => {
-    client.names = document.querySelector('#names').value;
-    client.email = document.querySelector('#email').value;
-    client.message = document.querySelector('#message').value;
+  client.names = document.querySelector('#names').value;
+  client.email = document.querySelector('#email').value;
+  client.message = document.querySelector('#message').value;
 
-    localStorage.setItem('content', JSON.stringify(client));
+  localStorage.setItem('content', JSON.stringify(client));
 });
 const dataOBJ = JSON.parse(localStorage.getItem('content'));
 if (dataOBJ) {
-    document.querySelector('#names').value = dataOBJ.names;
-    document.querySelector('#email').value = dataOBJ.email;
-    document.querySelector('#message').value = dataOBJ.message;
+  document.querySelector('#names').value = dataOBJ.names;
+  document.querySelector('#email').value = dataOBJ.email;
+  document.querySelector('#message').value = dataOBJ.message;
 }
