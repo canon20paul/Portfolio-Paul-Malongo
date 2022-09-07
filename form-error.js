c6f7da70c362a18298b00c9bd1f36876fb49cbd3
canon20paul <canon20paul@gmail.com>
2022-09-07 13:19:42 +0300

    Error handling complete

--- form-error.js
+++ form-error.js
@@ -1,7 +1,9 @@
 document.getElementById('contactF').addEventListener('submit', (event) => {
-    const email = document.getElementById('email').value;
+        const email = document.getElementById('email').value;
+        
     if (email !== email.toLowerCase()) {
         event.preventDefault();
         document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
     }
+    
 });
