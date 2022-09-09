f5eba8417be5296f55e8223b968d9de3b5974526
canon20paul <canon20paul@gmail.com>
2022-09-09 18:22:19 +0300

    formating

--- form-error.js
+++ form-error.js
@@ -10,9 +10,9 @@ document.getElementById('contactF').addEventListener('submit', (event) => {
 });
 document.getElementById('email').addEventListener('input', () => {
   function clear() {
-  document.querySelector('.error').innerHTML = "";
-  document.getElementById('email').style.color = '#111'
-  document.getElementById('email').style.borderBlockColor = '#d0d9d4'
-}
-clear();
+    document.querySelector('.error').innerHTML = '';
+    document.getElementById('email').style.color = '#111';
+    document.getElementById('email').style.borderBlockColor = '#d0d9d4';
+  }
+  clear();
 });
