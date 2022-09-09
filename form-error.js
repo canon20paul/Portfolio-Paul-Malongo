b095efcd5ac1d7539f6d59a7d1c73f8e44f69cf2
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-09 15:03:27 -0700

    Update validations

--- form-error.js
+++ form-error.js
@@ -8,3 +8,11 @@ document.getElementById('contactF').addEventListener('submit', (event) => {
     document.getElementById('email').style.borderBlockColor = 'red';
   }
 });
+document.getElementById('email').addEventListener('input', () => {
+  function clear() {
+  document.querySelector('.error').innerHTML = "";
+  document.getElementById('email').style.color = '#111'
+  document.getElementById('email').style.borderBlockColor = '#d0d9d4'
+}
+clear();
+});
